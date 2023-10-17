6bd8060eb0ba7ae7b4a71e43c399b7e0d7f8cc24
DenChenn <aaaa102234@gmail.com>
2023-10-17 17:08:15 +0800

    feat: initialize visisted array

--- maze/src/bfs.rs
+++ maze/src/bfs.rs
@@ -12,13 +12,15 @@ struct Walk {
 */
 
 pub fn bfs(maze: &Vec<Vec<i32>>) {
-    let y_length = maze.len() as usize;
-    let x_length = maze[0].len() as usize;
-    let mut visited: Vec<Vec<i32>> = Vec::with_capacity(y_length * x_length);
+    let y_length = maze.len();
+    let x_length = maze[0].len();
+    let mut visited: Vec<Vec<i32>> = Vec::with_capacity(y_length);
     for y in 0..y_length {
+        let mut x_list: Vec<i32> = Vec::with_capacity(x_length);
         for x in 0.. x_length {
-            visited[y][x] = 0
+            x_list.push(0)
         }
+        visited.push(x_list);
     }
 
     visited.into_iter().for_each(|it| {
